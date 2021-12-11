fda84bded08817df490a363b32bfd54b7f40ceed
julian <Joolean14>
2021-12-11 10:24:18 -0500

    [Feature] Route to detail page and add 404 page

---
+++ src/components/DetalleCancion.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function DetalleCancion() {
+    return (
+        <div>
+            <p>Estoy en detalle!</p>
+            {/* <p>{artista}</p>
+            <p>{album}</p>
+            <p>{nombre}</p>
+            <p>{categoria}</p>
+            <p>{sayco}</p>
+            <p>{acinpro}</p>
+            <img src={imagen}></img>             */}
+        </div>
+    )
+}
